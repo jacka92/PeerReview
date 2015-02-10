11321c3eb89ef07795cace77c4f69b006bd82dee
Jack <jack.armstrong92@gmail.com>
2015-02-10 15:55:07 +0000

    With basic var dumping from db to php/html file

--- dg_project/public_html/index.php
+++ dg_project/public_html/index.php
@@ -1,18 +1,24 @@
-
-   <body>
-        <?php
-    $connection = mysqli_connect('localhost','jack','Falconer92','peer_assessment')
-    or die('Error: '.mysql_error());
-?>
-
-
-
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
+<?php
+    $connection = mysqli_connect('localhost','jack','Falconer92','peer_assessment')
+        or die('Error: '.mysql_error());
+    $query = "SELECT * FROM assessments";
+    $result = mysqli_query($connection, $query)
+        or die ('Error: '.mysql_error());
+    while($row = mysqli_fetch_row($result)){
+        var_dump($row);
+        echo $row;
+    }    
+?>
+
+
+
 
 <html>
 
@@ -22,11 +28,15 @@ and open the template in the editor.
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href='css/bootstrap.min.css' rel='stylesheet'>
     </head>
-
-        <h1>Bullshit fucking database</h1>
-        <h2>These are the pieces of shits involved in this stupid application</h2>
-        <p>Pejhmon asshole Kamaie</p>
-        <p>Jack weak-as-fuck Armstrong</p>
-        <p>Samuel name-complex Partridge</p>
-    </body>
+        <body>
+            <h1>Bullshit fucking database</h1>
+            <h2>These are the pieces of shits involved in this stupid application</h2>
+            <p>Pejhmon asshole Kamaie</p>
+            <p>Jack weak-as-fuck Armstrong</p>
+            <p>Samuel name-complex Partridge</p>
+        </body>
 </html>
+
+<?php
+    mysqli_close($connection);
+?>
