6b5a83fb16faf2f085b363d0d986685d29cc6e56
Jack <jack.armstrong92@gmail.com>
2015-02-10 15:25:13 +0000

    swapped index.html for index.php

---
+++ Database Group Project/public_html/index.php
@@ -0,0 +1,32 @@
+
+   <body>
+        <?php
+    $connection = mysqli_connect('localhost','jack','Falconer92','peer_assessment')
+    or die('Error: '.mysql_error());
+?>
+
+
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='css/bootstrap.min.css' rel='stylesheet'>
+    </head>
+
+        <h1>Bullshit fucking database</h1>
+        <h2>These are the pieces of shits involved in this stupid application</h2>
+        <p>Pejhmon asshole Kamaie</p>
+        <p>Jack weak-as-fuck Armstrong</p>
+        <p>Samuel name-complex Partridge</p>
+    </body>
+</html>
