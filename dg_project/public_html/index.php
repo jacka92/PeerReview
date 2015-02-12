d9163c68b0693cbb774d8293375c7669eb996cae
Jack <jack.armstrong92@gmail.com>
2015-02-12 15:55:00 +0000

    Login added to index.html and php qury file added

--- dg_project/public_html/index.php
+++ dg_project/public_html/index.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 
 <?php
-    $connection = mysqli_connect('localhost','user','datacunts4lyf','peer_assessment')
+    $connection = mysqli_connect('localhost','root','','peer_assessment')
         or die('Error: '.mysql_error());
     $query = "SELECT * FROM assessments";
     $result = mysqli_query($connection, $query)
@@ -21,19 +21,53 @@ and open the template in the editor.
 
 
 <html>
-
+    
     <head>
+        
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href='css/bootstrap.min.css' rel='stylesheet'>
+<style>
+
+        </style>
+
     </head>
+
         <body>
             <h1>Bullshit fucking database</h1>
             <h2>These are the pieces of shits involved in this stupid application</h2>
             <p>Pejhmon asshole Kamaie</p>
             <p>Jack weak-as-fuck Armstrong</p>
             <p>Samuel name-complex Partridge</p>
+            <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+                <tr>
+                    <form name="form1" method="post" action="checklogin.php">
+                        <td>
+                            <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+                                <tr>
+                                    <td colspan="3"><strong>Member Login </strong></td>
+                                </tr>
+                                <tr>
+                                    <td width="78">Username</td>
+                                    <td width="6">:</td>
+                                    <td width="294"><input name="myusername" type="text" id="myusername"></td>
+                                </tr>
+                                <tr>
+                                    <td>Password</td>
+                                    <td>:</td>
+                                    <td><input name="mypassword" type="text" id="mypassword"></td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td>&nbsp;</td>
+                                    <td><input type="submit" name="Submit" value="Login"></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </form>
+                </tr>
+            </table>
         </body>
 </html>
 
