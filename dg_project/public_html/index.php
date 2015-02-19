9789f68fe52b79ead6ed2ff4697ed44f057f2eb8
Pejh <pejhmon@gmail.com>
2015-02-19 15:27:30 +0000

    template changes

--- dg_project/public_html/index.php
+++ dg_project/public_html/index.php
@@ -10,62 +10,56 @@ and open the template in the editor.
     <head>
         
         <title>Peer Assessment</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href='css/bootstrap.min.css' rel='stylesheet'>
-    <style>
-
-
-
-
-    </style>
+        <?php include 'templates/header imports.php';?>
 
     </head>
 
-        <body>
+    <body role='document'>
 
-            <h1>Bullshit fucking database</h1>
-            <h2>These are the pieces of shits involved in this stupid application</h2>
-            <p>Pejhmon asshole Kamaie</p>
-            <p>Jack weak-as-fuck Armstrong</p>
-            <p>Samuel name-complex Partridge</p>
-            <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-                <tr>
-                    <form name="form1" method="post" action="assessments.php"> <!-- TODO replace -->
-                        <td>
-                            <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-                                <tr>
-                                    <td colspan="3"><strong>Member Login </strong></td>
-                                </tr>
-                                <tr>
-                                    <td width="78">Username</td>
-                                    <td width="6">:</td>
-                                    <td width="294"><input name="myusername" type="text" id="myusername"></td>
-                                </tr>
-                                <tr>
-                                    <td>Password</td>
-                                    <td>:</td>
-                                    <td><input name="mypassword" type="text" id="mypassword"></td>
-                                </tr>
-                                <tr>
-                                    <td>&nbsp;</td>
-                                    <td>&nbsp;</td>
-                                    <td><input type="submit" name="Submit" value="Login"></td>
-                                </tr>
-                                <tr>
-                                    
-                                </tr>
+        <?php include 'templates/template header.php';?>
+
+        <h1>Bullshit fucking database</h1>
+        <h2>These are the pieces of shits involved in this stupid application</h2>
+        <p>Pejhmon asshole Kamaie</p>
+        <p>Jack weak-as-fuck Armstrong</p>
+        <p>Samuel name-complex Partridge</p>
+        <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+            <tr>
+                <form name="form1" method="post" action="assessments.php"> <!-- TODO replace -->
+                    <td>
+                        <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+                            <tr>
+                                <td colspan="3"><strong>Member Login </strong></td>
+                            </tr>
+                            <tr>
+                                <td width="78">Username</td>
+                                <td width="6">:</td>
+                                <td width="294"><input name="myusername" type="text" id="myusername"></td>
+                            </tr>
+                            <tr>
+                                <td>Password</td>
+                                <td>:</td>
+                                <td><input name="mypassword" type="text" id="mypassword"></td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td><input type="submit" name="Submit" value="Login"></td>
+                            </tr>
+                            <tr>
                                 
-                            </table>
-                            <a href = "registration.php">Register account</a>
-                        </td>
-                    </form>
-                    
-                </tr>
+                            </tr>
+                            
+                        </table>
+                        <a href = "registration.php">Register account</a>
+                    </td>
+                </form>
                 
-            </table>
+            </tr>
             
+        </table>
+        
 
-            
-        </body>
+        <?php include 'templates/template footer.php';?>
+    </body>
 </html>
---
+++ dg_project/public_html/templates/template footer.php
@@ -0,0 +1,11 @@
+<?php
+	echo "
+
+		<p> test footer for you cunts </p>
+
+
+		<!-- Bootstrap core JavaScript -->
+		<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
+		<script src='js/bootstrap.min.js'></script>
+	";
+?>
