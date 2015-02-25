87d93d67f9e2438fd9b5a6e47b8d012a8059121f
Pejh <pejhmon@gmail.com>
2015-02-25 11:54:42 +0000

    DB connection test

--- dg_project/public_html/db_connection.php
+++ dg_project/public_html/db_connection.php
@@ -1,4 +1,21 @@
 <?php
+
+// SQL Server test from internet
+/*
+$server = "tcp:<value of SERVER from section above>";
+$user = "<value of USERNAME from section above>"@SERVER_ID;
+$pwd = "password";
+$db = "testdb";
+
+$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+
+if($conn === false){
+    die(print_r(sqlsrv_errors()));
+}
+*/
+// Localhost test
+
+/*
 ////Use constants here instead of variables
 $dbhost = 'localhost';
 $dbuser = 'root';
@@ -14,4 +31,24 @@ if (mysqli_connect_errno()){
 			." (" .mysqli_connect_errno(). ")"
 	);
 }
+*/
+
+// SQL Server test from Azure's site
+
+try {
+   $conn = new PDO ( "sqlsrv:server = tcp:hvjcgi9sw1.database.windows.net,1433; Database = peerreview", "peerdataadmin", "datacunts4L!FE");
+       $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch ( PDOException $e ) {
+    print( "Error connecting to SQL Server." );
+    die(print_r($e));
+}
+          
+/*
+$connectionInfo = array("UID" => "peerdataadmin@hvjcgi9sw1", "pwd" => "datacunts4L!FE", "Database" => "peerreview", "LoginTimeout" => 30, "Encrypt" => 1);
+$serverName = "tcp:hvjcgi9sw1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+*/
+
+
 ?>
