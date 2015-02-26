63fab1fc80d23e53dcf8f1ae46dcd8b7ec452b1c
Pejh <pejhmon@gmail.com>
2015-02-26 17:49:07 +0000

    Site navigation

--- templates/template header.php
+++ templates/template header.php
@@ -1,8 +1,12 @@
 <?php
-/*
-require_once 'db_connection.php';
-session_start();
-*/
+try {
+   $conn = new PDO ( "sqlsrv:server = tcp:hvjcgi9sw1.database.windows.net,1433; Database = peerreview", "peerdataadmin", "datacunts4L!FE");
+       $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch ( PDOException $e ) {
+    print( "Error connecting to SQL Server." );
+    die(print_r($e));
+}
 ?>
 <!-- Fixed navbar -->
 <nav class='navbar navbar-inverse navbar-fixed-top'>
