b51df4abc204cb93ad18d26168c9cfb7f84b434d
sjmp <samueljamespartridge@gmail.com>
2015-03-12 17:47:19 +0000

    Fixing manual conflicts

--- viewreport.php
+++ viewreport.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'templates/db_connection.php';
-<<<<<<< HEAD
+
 session_start ();
 // TODO change this for a dynamic update
 $Group_ID = $_SESSION ['group_id'];
@@ -26,56 +26,16 @@ if (! $result) {
 	die ( "Database query failed." );
 }
 
-=======
-    
-    //TODO change this for a dynamic update
-    $group_id = 2;
-    
-    //query handling
-    $query = "SELECT * "; 
-    $query .= "FROM reports ";
-    
-    $result = mysqli_query($connection, $query);
-    
-    //query error handling
-    if (!$result){
-        die("Database query failed.");
-    }
-    
->>>>>>> 2d55940645dd6b5a0b8bfe683dcf3cd9a9f92d22
+
 ?>
 
 
 <html>
-<<<<<<< HEAD
 
-
-<head>
-<link rel="stylesheet" href="jquery-ui-1.11.3/jquery-ui.css">
-<script src="jquery-ui-1.11.3/external/jquery/jquery.js"></script>
-<script src="jquery-ui-1.11.3/jquery-ui.js"></script>
-            
-        
-        
-        <script type=text/javascript>
-          
- 
-        
-        </script>
-
-
-
-<title>Peer Assessment</title>
-        <?php
-								include 'templates/imports.php';
-								
-								?>
-=======
     <head>
         <link rel="stylesheet" href="jquery-ui-1.11.3/jquery-ui.css">
         <title>Peer Assessment</title>
         <?php include 'templates/imports.php';?>
->>>>>>> 2d55940645dd6b5a0b8bfe683dcf3cd9a9f92d22
 
     </head>
 
@@ -84,7 +44,7 @@ if (! $result) {
         <?php include 'templates/template_header.php';?>
         
         <style>
-<<<<<<< HEAD
+
 #accordion {
 	padding: 20px;
 	width: 30%;
@@ -98,62 +58,47 @@ if (! $result) {
 </style>
 
 	</div>
-=======
-            #accordion {
-                padding: 20px;
-                width: 30%;
-                float: left;
-            }
-            
-            #info {
-                width: 70%;
-                float: right;
-            }
-            
-        </style>            
->>>>>>> 2d55940645dd6b5a0b8bfe683dcf3cd9a9f92d22
+
 
 
 	<h1>View Reports</h1>
 	<h2><?php
-								
-								echo $Warning;
-								
-								?></h2>
+        echo $Warning;
+        ?></h2>
         
 
             <?php
-<<<<<<< HEAD
-												// run through all rows
-												echo "<div id ='accordion'>";
-												
-												echo "<h3> Create new report </h3>";
-												echo "<div><p>No report found. Create this report to the right.</p></div>";
-												while ( $row = mysqli_fetch_assoc ( $result ) ) {
-													// set the current row to this php var. Needed for id use
-													$currentrowgroupid = $row ["group_id"];
-													// giving all
-													// echo "<a href = javascript:select('$currentrowgroupid')>";
-													echo "<h3>" . "Report ID : " . $row ["report_id"] . "</h3>";
-													
-													// if the current row is your user'
-													if ($currentrowgroupid == $Group_ID) {
-														echo "<div style = color:#A00000 ";
-														// else just close the div
-													} else {
-														echo "<div ";
-													}
-													// set the div's id to the current row's group id
-													echo "id = $currentrowgroupid   >";
-													echo "<p> Report ID : " . $row ["report_id"] . "</li>";
-													echo "<p> Group ID: " . $row ["group_id"] . "</li>";
-													echo "<p> Mark Aggregate: " . $row ["mark_aggregate"] . "</li>";
-													echo "</div>";
-													// echo "</a>";
-												}
-												echo "</div>";
-												
-												?>
+
+                // run through all rows
+                echo "<div id ='accordion'>";
+
+                echo "<h3> Create new report </h3>";
+                echo "<div><p>No report found. Create this report to the right.</p></div>";
+                while ( $row = mysqli_fetch_assoc ( $result ) ) {
+                    // set the current row to this php var. Needed for id use
+                    $currentrowgroupid = $row ["group_id"];
+                    // giving all
+                    // echo "<a href = javascript:select('$currentrowgroupid')>";
+                    echo "<h3>" . "Report ID : " . $row ["report_id"] . "</h3>";
+
+                    // if the current row is your user'
+                    if ($currentrowgroupid == $Group_ID) {
+                        echo "<div style = color:#A00000 ";
+                        // else just close the div
+                    } else {
+                        echo "<div ";
+                    }
+                    // set the div's id to the current row's group id
+                    echo "id = $currentrowgroupid   >";
+                    echo "<p> Report ID : " . $row ["report_id"] . "</li>";
+                    echo "<p> Group ID: " . $row ["group_id"] . "</li>";
+                    echo "<p> Mark Aggregate: " . $row ["mark_aggregate"] . "</li>";
+                    echo "</div>";
+                    // echo "</a>";
+                }
+                echo "</div>";
+
+                ?>
 		<div id="info">
         <form method="post" id="report" action="viewreport.php">
 		<textarea name = "body" id="body" rows="20" cols="100" placeholder="Place your report body here" >
@@ -164,69 +109,15 @@ if (! $result) {
 		</form>
 		<input id="mark" type="hidden">
 	</div>
+    
+    <?php
+        include 'templates/template footer.php';
+    ?>
+    
+    <script src="jquery-ui-1.11.3/external/jquery/jquery.js"></script>
+    <script src="jquery-ui-1.11.3/jquery-ui.js"></script>
               
-=======
-                echo "<div id ='accordion'>";
-                    
-                    //First div
-                    echo "<h3> Create new report </h3>";
-                    echo "<div><p>No report found. Create this report to the right.</p></div>";
 
-                    //Loop to run through all results
-                    while($row = mysqli_fetch_assoc($result)){
-
-                        //setting current row as var
-                        $currentrowgroupid = $row["group_id"];
-
-                        //creating the header for each div
-                        echo "<h3>"."Report ID : ". $row["report_id"]. "</h3>"; 
-                        
-                        //if the current row is your user's id
-                        if ($currentrowgroupid==$group_id){
-                            echo "<div style = color:#A00000 ";
-
-                        //else close as normal
-                        }else{
-                            echo "<div ";    
-                        }
-
-                        //set the div's id to the current row's group id
-                        echo "id = $currentrowgroupid   >"; 
-                        
-                        //set out data in each row
-                        echo "<p> Report ID : ". $row["report_id"]. "</li>"; 
-                        echo "<p> Group ID: ". $row["group_id"]. "</li>";
-                        echo "<p> Mark Aggregate: ". $row["mark_aggregate"]. "</li>";
-                        echo "</div>";
-                        
-                        echo $row['report_text'];
-                    }
-
-                echo "</div>";
-        
-            ?>
-        
-        <!-- The central menu-->
-        <div id = "info">
-        <h2 id = "title">Select a report from the right</h2>
-        <h3 id = "subtitle">This'll be for group no</h3>
-        <br>
-        <textarea id="body" rows="20" cols="100" placeholder="Place your repory body here"></textarea>
-        <br/><br/>
-        <form action="dashboard.php"><input type = "submit" value = "Submit Report"></form>
-            <input id = "mark" type = "hidden">
-        </div>
-        
-        <!-- footer -->
-        <?php 
-            include 'templates/template footer.php';
-        ?>
-        
-        <!-- Script calls for jquery. Do not move! -->
-        <script src="jquery-ui-1.11.3/external/jquery/jquery.js"></script>
-        <script src="jquery-ui-1.11.3/jquery-ui.js"></script>
-        
->>>>>>> 2d55940645dd6b5a0b8bfe683dcf3cd9a9f92d22
     </body>
 </html>
 
@@ -283,15 +174,7 @@ if (! $result) {
         
         </script>
 
-<<<<<<< HEAD
 
-
-
-
-
-
-=======
->>>>>>> 2d55940645dd6b5a0b8bfe683dcf3cd9a9f92d22
 <?php
 
 mysqli_close ( $connection );
