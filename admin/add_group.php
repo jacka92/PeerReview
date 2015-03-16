3260f1939453caa97c6be72eb2ccba7f3834ed24
Pejh <pejhmon@gmail.com>
2015-03-16 14:21:09 +0000

    Admin + Forum

--- admin/add_group.php
+++ admin/add_group.php
@@ -3,7 +3,7 @@
 	include '../included_functions.php';
 
     $q  = "INSERT INTO groups (group_id) VALUES (NULL)";
-    $check = mysqli_query($connection, $q);
+    $check = mysqli_query($connection, $q)
             or die ('Error: insert failed'.mysql_error());  
     confirm_query($check);
 
